630e4f378822383c075a443c510172178e8ac7d9
Mazen Ahmed <mazenahmed96@gmail.com>
2022-10-02 17:34:08 +0200

    Update IP-Checker-AbuseIPDB.py

--- IP-Checker-AbuseIPDB.py
+++ IP-Checker-AbuseIPDB.py
@@ -3,7 +3,7 @@
     Mazen Ahmed Gaballah
     ---------------------------
     this is to check bulk of IPs reputation regarding to AbuseIPDB feeds
-    Input is a CSV file containing list of IPs to be searched.
+    Input is a CSV file named 'ips.csv' which containing list of IPs to be searched.
     Output is a CSV file with all information about the IP.
     https://docs.abuseipdb.com
 '''
